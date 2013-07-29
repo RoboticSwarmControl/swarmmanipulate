94769124316413b6265e9b7e533705f689b30c1b
Chris Ertel <crertel@fancybastard>
2013-07-29 18:03:54 -0500

    Bad scaling fix.

--- app/assets/javascripts/core/results.js
+++ app/assets/javascripts/core/results.js
@@ -13,17 +13,27 @@ swarmcontrol.results = (function () {
 
             // ...and add the data points for the graph...
             var points = [];
+            var tmax=Number.MIN_VALUE, tmin=Number.MAX_VALUE, rmax=Number.MIN_VALUE, rmin=Number.MAX_VALUE;
+
             _.each( res, function (r) {
+                
+                tmax = tmax < r.runtime ? r.runtime : tmax;
+                tmin = tmin > r.runtime ? r.runtime : tmax;
+                rmax = rmax < r.robot_count ? r.robot_count : rmax;
+                rmin = rmin > r.robot_count ? r.robot_count : rmin;
+
                 points.push( [r.robot_count, r.runtime] );
             });
 
-            // ...and then append the graph.
-             
+            // ...and then append the graph.             
             Flotr.draw( $task[0],
                         [
                             {data: points, label: 'labals!', points: {show:true}}
                         ],
-                        {});
+                        {
+                            xaxis: { min: .9*rmin, max: 1.1*rmax},
+                            yaxis: { min: .9*tmin, max: 1.1*tmax} 
+                        });
         });
     };
     return { init: init

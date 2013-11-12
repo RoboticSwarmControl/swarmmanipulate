4c36404ccf8d875d16662a07014423f897150774
Aaron Becker <aabecker@gmail.com>
2013-11-12 18:22:29 -0500

    Still homing in on offset

--- app/assets/javascripts/core/globalController.js
+++ app/assets/javascripts/core/globalController.js
@@ -44,10 +44,10 @@ var globalController = (function(){
             var rect = this.getBoundingClientRect();
             var touch = e.touches[0];
             var left = touch.pageX - rect.left - this.clientLeft + this.scrollLeft;
-            var top = touch.pageY - rect.top - this.clientTop + this.scrollTop + 5;
+            var top = touch.pageY - rect.top - this.clientTop + this.scrollTop;
 
             that._mX = 20 * left/this.width;
-            that._mY = 20 * top/this.height;
+            that._mY = 20 * top/this.height -5;
         },false);        
 
         $("#canvas")[0].addEventListener('touchstart', function(e) {

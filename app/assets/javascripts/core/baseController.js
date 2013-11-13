0f14b176f319187b8a321ad1dd3bcd779ba5db7b
Aaron Becker <aabecker@gmail.com>
2013-11-12 22:28:42 -0500

    Implemented tilt event for window.
    Must verify with ipad

--- app/assets/javascripts/core/baseController.js
+++ app/assets/javascripts/core/baseController.js
@@ -1,32 +1,5 @@
 
-        function tilt(that,yval, xval){
-            // simple control that maps tilt to keypad values.
-            var thres = 20;
-            that.keyL=null;
-            that.keyR=null;
-            that.keyU=null;
-            that.keyD=null;
-            that.lastUserInteraction = new Date().getTime();
 
-            if( Math.abs( yval) + Math.abs(xval) > thres)
-            {
-                if( yval > thresh )
-                {that.keyL = that.lastUserInteraction;}
-                else if ( yval < -thresh )
-                {that.keyR = that.lastUserInteraction;}
-
-                if( xval > thresh )
-                {that.keyU = that.lastUserInteraction;}
-                else if ( xval < -thresh )
-                {that.keyD = that.lastUserInteraction;}
-            }
-            //check if this is the first valid keypress, if so, starts the timer
-            if( that._startTime == null && ( that.keyL != null || that.keyR != null || that.keyU != null || that.keyD != null))
-            { 
-                that._startTime = that.lastUserInteraction;
-                that._runtime = 0.0;
-            }
-        }
 
 
 
@@ -61,22 +34,55 @@ var baseController = (function(){
         } , false );
 
 
-        if (document.DeviceOrientationEvent) {
-            document.addEventListener("deviceorientation", function () {
-                tilt(that,[event.beta, event.gamma]);
-                //beta is -90 (top down) to +90  (top up), gamma is -90 (left up) to +90 (right up)
-            }, true);
-        } else if (document.DeviceMotionEvent) {
-            document.addEventListener('devicemotion', function () {
-                tilt(that,[event.acceleration.x * 2, event.acceleration.y * 2]);
-                //https://developer.mozilla.org/en-US/docs/Web/Reference/Events/devicemotion
-            }, true);
-        } else {
-            document.addEventListener("MozOrientation", function () {
-                tilt(that,[orientation.x * 50, orientation.y * 50]);
-                //The X axis represents the amount of right-to-left tilt. This value is 0 if the device is level along the X axis, and approaches 1 as the device is tilted toward the left, and -1 as the device is tilted toward the right.
-                //The Y axis represents the amount of front-to-back tilt. The value is 0 if the device is level along the Y axis, and approaches 1 as you tilt the device backward (away from you) and -1 as you tilt the device frontward (toward you).
-            }, true);
+        if (window.DeviceOrientationEvent) { // mobile devices with accelerometers can tilt the device to move robots
+            window.addEventListener('deviceorientation', function(event) {
+                var xval = event.beta;  // In degree in the range [-180,180]
+                var yval = event.gamma; // In degree in the range [-90,90]
+
+                //property may change. A value of 0 means portrait view, 
+                if( window.orientation == -90)
+                {   //-90 means a the device is landscape rotated to the right,
+                    xval = event.gamma;
+                    yval = event.beta; 
+                }else if( window.orientation == 90)
+                {   //and 90 means the device is landscape rotated to the left.
+                    xval = event.gamma;
+                    yval =-event.beta; 
+                }
+                 
+                // simple control that maps tilt to keypad values.
+                var thres = 20;
+                that.lastUserInteraction = new Date().getTime();
+     
+                if( yval > thresh )
+                {   
+                    that.keyD=null;
+                    if(that.keyU==null){that.keyU = that.lastUserInteraction;} 
+                }else if ( yval < -thresh )
+                {   
+                    that.keyU=null;
+                    if(that.keyD==null){that.keyD = that.lastUserInteraction;} 
+                }else
+                {that.keyD=null; that.keyU=null;}
+
+                if( xval > thresh )
+                {   
+                    that.keyR=null;
+                    if(that.keyL==null){that.keyL = that.lastUserInteraction;} 
+                }else if ( xval < -thresh )
+                {   
+                    that.keyL=null;
+                    if(that.keyR==null){that.keyR = that.lastUserInteraction;} 
+                }else
+                {that.keyR=null; that.keyL=null;}
+            
+                //check if this is the first valid keypress, if so, starts the timer
+                if( that._startTime == null && ( that.keyL != null || that.keyR != null || that.keyU != null || that.keyD != null))
+                { 
+                    that._startTime = that.lastUserInteraction;
+                    that._runtime = 0.0;
+                }
+            });
         }
 
 
